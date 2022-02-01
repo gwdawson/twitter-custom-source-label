a0ce6c5457cd673e9cfdfda1104155375c26e69f
gwdawson <gwdawson.work@gmail.com>
2022-02-01 00:17:14 +0000

    Add framework to rewrite

---
+++ rewrite.py
@@ -0,0 +1,62 @@
+import tweepy
+import os
+
+os.system("clear")
+
+
+custom_source_labels = [
+    {
+        "Application Name": "",
+        "API KEY": "",
+        "API KEY SECRET": "",
+        "ACCESS TOKEN": "",
+        "ACCESS TOKEN SECRET": "",
+    },
+    {
+        "Application Name": "",
+        "API KEY": "",
+        "API KEY SECRET": "",
+        "ACCESS TOKEN": "",
+        "ACCESS TOKEN SECRET": "",
+    },
+    {
+        "Application Name": "",
+        "API KEY": "",
+        "API KEY SECRET": "",
+        "ACCESS TOKEN": "",
+        "ACCESS TOKEN SECRET": "",
+    },
+]
+
+
+def about_the_software():
+    print("about_the_software")
+    return
+
+
+def select_source_label():
+    print("select_source_label")
+    return
+
+
+def write_your_tweet():
+    print("write_your_tweet")
+    return
+
+
+def select_youe_image():
+    print("select_youe_image")
+    return
+
+
+def review_your_tweet():
+    print("review_your_tweet")
+    return
+
+
+def send_the_tweet():
+    print("send_the_tweet")
+    return
+
+
+about_the_software()
