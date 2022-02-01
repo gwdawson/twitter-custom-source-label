7414504170349501cfa294f84eefe1adcfca05b7
gwdawson <gwdawson.work@gmail.com>
2022-02-01 01:03:42 +0000

    Update about_the_software()

--- rewrite.py
+++ rewrite.py
@@ -29,8 +29,20 @@ custom_source_labels = [
 ]
 
 
+about = """Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque possimus neque nobis animi quas ab delectus iure
+eligendi saepe provident, eveniet culpa magni, consectetur labore expedita consequatur ipsum voluptatum voluptas!
+Asperiores, ut ea accusamus debitis mollitia ex voluptatibus voluptates ipsam at nobis exercitationem assumenda
+magni sed fugit ipsa aut minus quidem neque! Facere hic, est eveniet porro magni culpa, repellendus maxime tenetur
+natus nisi animi ea distinctio consequatur quas fugiat ut deleniti aperiam? Saepe esse eligendi deserunt, aperiam
+atque sunt ut dolore libero assumenda debitis commodi quisquam eius nisi sed? Dicta reiciendis nihil quidem quasi
+vel saepe libero reprehenderit sunt."""
+
+
 def about_the_software():
-    print("about_the_software")
+    print(f"{about}\n")
+    input("Press Enter to continue...")
+    print("\n----------------------------------------------------------------------------------------------------\n")
+    select_source_label()
     return
 
 
